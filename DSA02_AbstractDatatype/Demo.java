f06052b9c3ed64009e0ce7647aa1e4aa736fe7ad
Mangeshshinde8308 <mangeshshinde8308@gmail.com>
2025-08-19 14:38:28 +0530

    ADT datactype

---
+++ DSA02_AbstractDatatype/Demo.java
@@ -0,0 +1,21 @@
+package DSA02_AbstractDatatype;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hellow");
+        //ADT is model for data structure that defines the behaviour of the data type without without specifying its implementation. 
+        //It focus on what operations data type can perform rather that how it implement.
+        //The List ADT need to store the required data in the sequence and should have the following operations:
+        //get(): Return an element from the list at any given position.
+        //insert(): Insert an element at any position in the list.
+         //remove(): Remove the first occurrence of any element from a non-empty list.
+
+
+
+        //Stack ADT, the order of insertion and deletion should be according to the FILO or LIFO Principle.
+        // Elements are inserted and removed from the same end, called the top of the stack. It should also support the following operations:
+        //push(): Insert an element at one end of the stack called the top.
+        //pop(): Remove and return the element at the top of the stack, if it is not empty.
+    }
+
+}
